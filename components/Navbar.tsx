a9a7efb9e8b6c468349a4e2d1c7f5f6c6a446dcd
Ajarek <ajarek@poczta.onet.pl>
2025-02-17 00:43:57 +0100

    start project

---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+
+const Navbar = () => {
+  return (
+    <nav className='h-16 flex justify-between items-center px-8 bg-primary'>
+      <div className='flex items-center space-x-2'>
+        <Image src="/images/logo.png" alt="Logo" width={50} height={50} />
+        <h1 className=' text-2xl font-bold'>Character Counter</h1>
+      </div>
+      <ModeToggle/>
+    </nav>
+  )
+}
+
+export default Navbar
